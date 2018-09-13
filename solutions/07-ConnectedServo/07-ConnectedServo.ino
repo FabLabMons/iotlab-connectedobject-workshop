373480e7b26abfebbe357361c9d6e0945067e78b
François Roland <fr.roland@gmail.com>
2018-09-13 23:37:51 +0200

    Write servo angle poller

---
+++ solutions/07-ConnectedServo/07-ConnectedServo.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Servo.h>
+
+const char* NODE_ID = "node-00";
+
+const int MIN_PULSE_WIDTH_US = 900;
+const int MAX_PULSE_WIDTH_US = 2100;
+const int PULSE_PERIOD_MS = 20;
+const int PULSE_STEP = 20;
+const char* ssid     = "MONS";
+const char* password = "Mons2017";
+
+Servo myservo;
+unsigned long lastChangeTimestamp = 0;
+unsigned long lastPingTimestamp = 0;
+int currentPulseWidth = MIN_PULSE_WIDTH_US;
+int desiredPulseWidth = currentPulseWidth;
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to WiFi ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  myservo.attach(D1);
+  myservo.writeMicroseconds(currentPulseWidth);
+}
+
+void loop() {
+  
+  if (millis() - lastChangeTimestamp > PULSE_PERIOD_MS) {
+    int delta = desiredPulseWidth - currentPulseWidth;
+    if (delta < - PULSE_STEP) {
+      currentPulseWidth -= PULSE_STEP;
+    } else if (delta > PULSE_STEP) {
+      currentPulseWidth += PULSE_STEP;
+    } else {
+      currentPulseWidth += delta;
+    }
+    myservo.writeMicroseconds(currentPulseWidth);
+    lastChangeTimestamp = millis();
+  }
+
+  if (millis() - lastPingTimestamp > 1000) {
+    HTTPClient http;
+    Serial.println("Opening connection");
+    http.begin("http://192.168.1.100/red/" + NODE_ID + "/angle");
+    http.GET();
+    String serverAnswer = http.getString();
+    http.end();
+    int desiredAngle =  serverAnswer.toInt();
+    desiredPulseWidth = map(desiredAngle, 0, 90, MIN_PULSE_WIDTH_US, MAX_PULSE_WIDTH_US);
+    lastPingTimestamp = millis();
+  }
+}
