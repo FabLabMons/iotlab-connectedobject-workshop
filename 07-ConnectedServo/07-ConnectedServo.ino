2dec588ee95078667cff46903ab77119a13c54cb
François Roland <fr.roland@gmail.com>
2018-09-16 11:54:17 +0200

    Design node-red ping flow

--- 07-ConnectedServo/07-ConnectedServo.ino
+++ 07-ConnectedServo/07-ConnectedServo.ino
@@ -2,26 +2,24 @@
 #include <ESP8266HTTPClient.h>
 #include <Servo.h>
 
+// Début configuration
 const char* NODE_ID = "node-00";
-
 const int MIN_PULSE_WIDTH_US = 900;
 const int MAX_PULSE_WIDTH_US = 2100;
-const int PULSE_PERIOD_MS = 20;
-const int PULSE_STEP = 20;
+// Fin configuration
+
+const int ANGLE_DELAY_MS = 20;
+const int ANGLE_STEP = 5;
 const char* ssid     = "MONS";
 const char* password = "Mons2017";
 
 Servo myservo;
-unsigned long lastChangeTimestamp = 0;
-unsigned long lastPingTimestamp = 0;
-int currentPulseWidth = MIN_PULSE_WIDTH_US;
-int desiredPulseWidth = currentPulseWidth;
+String url;
+int currentAngle = 0;
+int desiredAngle = 0;
 
 void setup() {
-  Serial.begin(9600);
-  delay(1000);
-
-  Serial.println();
+  Serial.begin(115200);
   Serial.println();
   Serial.print("Connecting to WiFi ");
   Serial.println(ssid);
@@ -39,35 +37,42 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  myservo.attach(D1);
-  myservo.writeMicroseconds(currentPulseWidth);
+  myservo.attach(D1, MIN_PULSE_WIDTH_US, MAX_PULSE_WIDTH);
+  myservo.write(0);
   delay(300);
+
+  url = String("http://192.168.1.22/red/") + NODE_ID + "/angle";
 }
 
 void loop() {
   
-  if (millis() - lastChangeTimestamp > PULSE_PERIOD_MS) {
-    int delta = desiredPulseWidth - currentPulseWidth;
-    if (delta < - PULSE_STEP) {
-      currentPulseWidth -= PULSE_STEP;
-    } else if (delta > PULSE_STEP) {
-      currentPulseWidth += PULSE_STEP;
-    } else {
-      currentPulseWidth += delta;
-    }
-    myservo.writeMicroseconds(currentPulseWidth);
-    lastChangeTimestamp = millis();
-  }
+  HTTPClient http;
 
-  if (millis() - lastPingTimestamp > 1000) {
-    HTTPClient http;
-    String url = String("http://192.168.1.22/red/") + NODE_ID + "/angle";
-    http.begin(url);
-    http.GET();
-    String serverAnswer = http.getString();
-    http.end();
-    int desiredAngle =  serverAnswer.toInt();
-    desiredPulseWidth = map(desiredAngle, 0, 90, MIN_PULSE_WIDTH_US, MAX_PULSE_WIDTH_US);
-    lastPingTimestamp = millis();
+  http.begin(url);
+  http.GET();
+  String serverAnswer = http.getString();
+  desiredAngle =  serverAnswer.toInt();
+  http.end();
+
+  int delta = desiredAngle - currentAngle;
+  if (delta != 0) {
+    Serial.print("Moving to angle ");
+    Serial.print(desiredAngle);
+    Serial.print(" ");
   }
+  while (delta != 0) {
+    if (delta > ANGLE_STEP) {
+      currentAngle = currentAngle + ANGLE_STEP;
+    } else if (delta < ANGLE_STEP) {
+      currentAngle = currentAngle - ANGLE_STEP;
+    } else {
+      currentAngle = currentAngle + delta;
+    }
+    myservo.write(currentAngle);
+    delay(ANGLE_DELAY_MS);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  delay(1000);
 }
