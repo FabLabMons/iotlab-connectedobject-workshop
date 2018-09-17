99701ad53fb34baf923b4975bca319bdda5e1cb4
François Roland <fr.roland@gmail.com>
2018-09-17 16:21:51 +0200

    Rewrite exercice 4 with a loop

--- 04-SetPosition/04-SetPosition.ino
+++ 04-SetPosition/04-SetPosition.ino
@@ -7,41 +7,38 @@ static const int PULSE_STEP = 20;
 
 Servo myservo;
 unsigned long lastChangeTimestamp = 0;
-int currentPulseWidth = MIN_PULSE_WIDTH_US;
-int desiredPulseWidth = currentPulseWidth;
+int currentAngle = MIN_PULSE_WIDTH_US;
+int desiredAngle = currentAngle;
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
-  myservo.attach(D1);
-  myservo.writeMicroseconds(currentPulseWidth);
+  myservo.attach(D1, MIN_PULSE_WIDTH_US, MAX_PULSE_WIDTH_US);
+  myservo.writeMicroseconds(currentAngle);
   delay(300);
   Serial.println("Angle du servo (entre 0 et 90) ? ");
 }
 
 void loop() {
   
-  if (millis() - lastChangeTimestamp > PULSE_PERIOD_MS) {
-    int delta = desiredPulseWidth - currentPulseWidth;
+  int delta = desiredAngle - currentAngle;
+  while (delta != 0) {
     if (delta < - PULSE_STEP) {
-      currentPulseWidth -= PULSE_STEP;
+      currentAngle -= PULSE_STEP;
     } else if (delta > PULSE_STEP) {
-      currentPulseWidth += PULSE_STEP;
+      currentAngle += PULSE_STEP;
     } else {
-      currentPulseWidth += delta;
+      currentAngle += delta;
     }
-    myservo.writeMicroseconds(currentPulseWidth);
-    lastChangeTimestamp = millis();
+    myservo.writeMicroseconds(currentAngle);
+    delta = desiredAngle - currentAngle;
+    delay(PULSE_PERIOD_MS);
   }
 
   if (Serial.available() > 0) {
     int desiredAngle = Serial.parseInt();
-    if (Serial.read() == '\n') {
-      if (desiredAngle < 0 || desiredAngle > 90) {
-        Serial.println("Erreur. La valeur n'est pas comprise entre 0 et 90");
-      } else {
-        desiredPulseWidth = map(desiredAngle, 0, 90, MIN_PULSE_WIDTH_US, MAX_PULSE_WIDTH_US);
-      }
+    if (Serial.read() == '\n' && (desiredAngle < 0 || desiredAngle > 90)) {
+      Serial.println("Erreur. La valeur n'est pas comprise entre 0 et 90");
     }
   }
 }
